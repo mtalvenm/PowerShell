7a474974e95368a6d9ecda08001d0e0b12ec78db
Talvenmaa Mikko <mikko.talvenmaa@digia.com>
2017-10-23 13:56:40 +0300

    Commented functions

--- SCCM_Client.ps1
+++ SCCM_Client.ps1
@@ -1,8 +1,10 @@
+# queries client for current Site Code
 function getSCCMSite() {
     $sccm = New-Object -ComObject "Microsoft.SMS.Client"
     $sccm.getSiteAssignment()
 }
 
+# set new Site Code for client
 function setSCCMSite([String] $siteCode) {
     $sccm = New-Object -ComObject "Microsoft.SMS.Client"
     $sccm.setSiteAssignment($siteCode)
